ef992d9d4bc2005993b74d9aae3700bb3cbddece
ross dibartolo <dibartoloross@gmail.com>
2022-12-30 12:01:04 -0500

    Exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite class?");
+                var favClass = Console.ReadLine();
+                switch (favClass)
+            {
+                case "history":
+                    Console.WriteLine("History");
+                    break;
+                case "math":
+                    Console.WriteLine("Math");
+                    break;
+                case "science":
+                    Console.WriteLine("Science");
+                    break;
+                case "gym":
+                    Console.WriteLine("Gym");
+                    break;
+                case "foreign language":
+                    Console.WriteLine("Foreign Language");
+                    break;
+                default:
+                    Console.WriteLine("We don't have that class");
+                    break;
+            }
         }
     }
 }
